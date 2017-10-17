1f779dfe56a4acc3fc0d3a9a7670bdbe0f438268
oualidbouh <bouh.oualid@gmail.com>
2017-10-17 23:43:32 +0200

    reset password

--- newpassword.php
+++ newpassword.php
@@ -1,106 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+		<?php
+			echo file_get_contents("header.php");
+		?>
+		<body>
 <?php 
-	
+	session_start();
 	include('conf/db.inc');
 	$stmt = mysqli_stmt_init($mysqli);
-	$email = $_GET['user'];
-	$token = $_GET['token'];
-	
-	$query = "SELECT username FROM user WHERE token=? AND email =? AND active = 1";
-	if(mysqli_stmt_prepare($stmt,$query))
-	{
-		mysqli_stmt_bind_param($stmt,'ss',$token,$email);
-		mysqli_stmt_execute($stmt);
-		mysqli_stmt_bind_result($stmt,$username);
-		mysqli_stmt_fetch($stmt);
-		
-		if( !is_null($username) )
+
+	if(!isset($_POST['submit'])){
+		$email = $_GET['email'];
+		$token = $_GET['token'];
+		$query = "SELECT username FROM user WHERE token=? AND email =? AND active = 1";
+		if(mysqli_stmt_prepare($stmt,$query))
 		{
-			// user exist & token is correct ==> pwd shall be renewed
+			mysqli_stmt_bind_param($stmt,'ss',$token,$email);
+			mysqli_stmt_execute($stmt);
+			mysqli_stmt_bind_result($stmt,$username);
+			mysqli_stmt_fetch($stmt);
+		if( !is_null($username))
+		{
+			$_SESSION['username'] = $username;
+?>
+			<div class="container">
+				<div id="newpasword" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+					<div class="panel panel-info">
+						<div class="panel-heading">
+							<div class="panel-title">Saisir votre nouveau mot de passe</div>
+						</div>
+						<div style="padding-top:30px" class="panel-body" >
+							<form id="loginform" class="form-horizontal" role="form" action="<?=$_SERVER['PHP_SELF']?>" method="post">
+								<div class="form-group">
+                    				<label for="username" class="col-md-3 control-label">Nouveau mot de passe</label>
+                    					<div class="col-md-9">
+                        					<input name="newpwd" type="password" class="form-control" placeholder="Votre nouveau mot de passe" />
+                    					</div>
+                				</div>
+										<div class="form-group">
+                    						<label for="username" class="col-md-3 control-label">Confirmer le mot de passe</label>
+                    						<div class="col-md-9">
+                        						<input name="confirm-newpwd" type="password" class="form-control" placeholder="Confirmer le nouveau mot de passe"/>
+                    						</div>
+                						</div>
+										<div style="margin-top:10px" class="form-group">
+											<div class="col-sm-12 controls">
+												<input id="btn-signup" type="submit" class="btn btn-info" name="submit" type="submit" value="Se connecter"/></div>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div> 
+						</div>
+					</div>
+<?php
 			exit;
-		} else {
-			echo "Erreur d'identification";
+		}
+		else
+		{
+			echo "Email ou le token sont invalides.";
+		} 
+	}
+		else {
+			echo "Erreur dans la REQUETE SQL";
 			exit;
 		}
 	}
-
-
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-	<?php
-		echo file_get_contents("header.php");
-	?>
-
-	<body>
-
-		<?php
-			session_start();
-	
-			if(isset($_GET['action']) && ($_GET['action']=='logout'))
+	else
+	{
+		$password = $_POST['newpwd'];
+		$confirm = $_POST['confirm-newpwd'];
+		if($password == $confirm)
+		{
+			$options = [
+				'salt' => hash("sha256",$_SESSION['username']),
+			];
+			$password = password_hash ($password,PASSWORD_DEFAULT,$options);
+			$token = md5(rand());
+			$query = "UPDATE user SET token='$token' , password = '$password' WHERE username=? AND active = 1";
+			if(mysqli_stmt_prepare($stmt,$query))
 			{
-				session_destroy();
-			}
-
-			if(isset($_SESSION['error']))
-			{
-				echo "<div id=\"error-message\" style=\"margin-top:50px;\" class=\"mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2\">
-					  	<div class=\"panel panel-info\">
-							<div class=\"panel-heading\">
-								<div class=\"panel-title\">Erreur</div>";
-				echo "<p class=\"text-danger\">".$_SESSION['error']['username']."</p>";
-				echo "<p class=\"text-danger\">".$_SESSION['error']['email']."</p>";
-				echo "<p class=\"text-danger\">".$_SESSION['error']['password']."</p>";
-				echo "			</div>
-							</div>
-						</div>
-					  </div>";
-				unset($_SESSION['error']);
-			}
-
-			if(isset($_SESSION['username']))
-			{
-				header("Location: home.php");
-			} else {
-				if(isset($_POST['submit']))
+				mysqli_stmt_bind_param($stmt,'s',$_SESSION['username']);
+				mysqli_stmt_execute($stmt);
+				if( mysqli_affected_rows($mysqli) > 0)
 				{
-					include('conf/db.inc');
-					$stmt = mysqli_stmt_init($mysqli);
-					$username = trim($_POST['username']);
-					$password = trim($_POST['password']);
-					$token="";$active=0;
-					$options = [
-						'salt' => hash("sha256",$username),
-					];
-					$password = password_hash ($password ,PASSWORD_DEFAULT, $options);
-					//superadmin'# : SQLi
-					$query = "SELECT username, password,token,active FROM user WHERE username=? AND password =? ";
-					if(mysqli_stmt_prepare($stmt,$query))
-					{
-						mysqli_stmt_bind_param($stmt,'ss',$username,$password);
-						mysqli_stmt_execute($stmt);
-						mysqli_stmt_bind_result($stmt,$username,$password,$token,$active);
-						mysqli_stmt_fetch($stmt);
-						//echo (is_null($username));
-						
-						if( !is_null($username) && $active==1)
-						{
-							$_SESSION['username']=$username;
-							header("Location: home.php");
-							exit;
-						} else {
-							echo "Erreur d'identification";
-							exit;
-						}
-					}
-				} else {
-					echo file_get_contents("forms/login.php");
-					echo file_get_contents("forms/create.php");	
-					echo file_get_contents("forms/forgot_password.php");
+					
+					header('Location: index.php');
+				}
+				else
+				{
+					echo "token qdim";
 				}
 			}
-		?>
-
+		}
+		else
+		{
+			echo "passwords don't match";
+		}
+		session_destroy();
+	}
+?>
 	</body>
 </html>
